c5e033213ad185002af245570073f7c3636249f2
Adit Milano <adityacahya17@gmail.com>
2024-12-08 12:08:07 +0800

    new-revision

--- assests/scripts/signup.js
+++
@@ -1,5 +0,0 @@
-function showOtherForm() {
-    // Hide the first form and show the second form (Other info)
-    document.getElementById('signup-form').style.display = 'none';
-    document.getElementById('other-form').style.display = 'block';
-}
